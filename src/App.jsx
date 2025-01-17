bcc339ce0ff99ed7560da10a6739cc34c4378d06
Miguel Flores <abraham.80.af80@gmail.com>
2025-01-17 16:10:14 -0600

    [WIP] Add darkmode logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { getTheme } from "./Util/MuiTheme";
-/* import Button from "@mui/material/Button"; */
+import Button from "@mui/material/Button";
 import router from "./router/router"
 import "./App.css";
 
@@ -13,16 +13,25 @@ function App() {
 
   const theme = getTheme(darkMode);
 
-  /* const toggleDarkMode = () => setDarkMode(!darkMode); */
+  useEffect(() => {
+    const root = document.documentElement;
+
+    root.style.setProperty('--primary-color', theme.palette.primary.main);
+    root.style.setProperty('--secondary-color', theme.palette.secondary.main);
+    root.style.setProperty('--background-color', theme.palette.background.default);
+    root.style.setProperty('--paper-color', theme.palette.background.paper);
+  }, [theme]);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* <div style={{ textAlign: "center", padding: "1rem" }}>
+      <div style={{ textAlign: "center", padding: "1rem" }}>
         <Button variant="contained" onClick={toggleDarkMode}>
           Cambiar a {darkMode ? "Claro" : "Oscuro"}
         </Button>
-      </div> */}
+      </div>
       <RouterProvider router={router} />
     </ThemeProvider>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./loginReducer";
+import themeReducer from "./themeSlice"
 
 const store = configureStore({
     reducer: loginReducer
