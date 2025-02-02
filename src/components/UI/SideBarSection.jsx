3a73eb5faf331f05c16104586b6e4ca3b0141607
Miguel Flores <84428383+Mike1604@users.noreply.github.com>
2025-02-02 14:18:27 -0600

    [fix]Sidebar actions

--- src/components/UI/SideBarSection.jsx
+++ src/components/UI/SideBarSection.jsx
@@ -15,16 +15,19 @@ export default function SideBarSection({ title, icon: Icon, options }) {
 
   return (
     <div className="sidebar-section-cont" onClick={toggleSection}>
-        <div className="sidebar-section-title">
-          {Icon && <Icon />}
-          <h3>{title}</h3>
-          <ArrowRightIcon className={`${isOpen ? "icon-active" : ""}`}/>
+      <div className="sidebar-section-title">
+        {Icon && <Icon />}
+        <h3>{title}</h3>
+        <ArrowRightIcon className={`${isOpen ? "icon-active" : ""}`} />
       </div>
       <ul className={`sidebar-section-options ${isOpen ? "open" : ""}`}>
-        
         <hr className="divider" />
         {options.map((option, index) => (
-          <Link to={option.route} key={index}>
+          <Link
+            to={option.route}
+            key={index}
+            onClick={(e) => e.stopPropagation()}
+          >
             <li className="sidebar-option">
               <h2>{option.title}</h2>
             </li>
